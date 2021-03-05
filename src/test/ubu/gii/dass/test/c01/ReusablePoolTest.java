5b58f36aad1a124589721f6d6176cc2095a00343
Elsa <34100669+etc99@users.noreply.github.com>
2021-03-05 17:56:45 +0100

    Desarrollo de los métodos

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,6 +9,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.NotFreeInstanceException;
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
+
 /**
  * @author alumno
  *
@@ -34,15 +38,26 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
+		ReusablePool contenedor = null;
+		assertNull(contenedor);
+		assertNotNull(contenedor.getInstance());
 	}
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
+	 * @throws NotFreeInstanceException 
 	 */
 	@Test
-	public void testAcquireReusable() {
-		fail("Not yet implemented");
+	public void testAcquireReusable() throws NotFreeInstanceException {
+		ReusablePool cont2 = ReusablePool.getInstance();
+		Reusable r1,r2 = null;
+		
+		r1 = cont2.acquireReusable();
+		assertNotNull(r1);
+		r2 = cont2.acquireReusable();
+		assertNotNull(r2);
+		//este método devuelve una excepcion
+		//assertError(cont2.acquireReusable());
 	}
 
 	/**
@@ -50,7 +65,18 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testReleaseReusable() {
-		fail("Not yet implemented");
+		ReusablePool cont3 = ReusablePool.getInstance();
+		Reusable r1,r2 = null;
+		r1 = cont3.acquireReusable();
+		String r1util = r1.util(); //guardamos su r1.util()
+		assertNotNull(r1);
+		cont3.releaseReusable(r1);//si volvemos a crear otro deberia coincidir so .til
+		r2 = cont3.acquireReusable();
+		assertNotNull(r1);
+		//Assert.assertEquals(r1util, r2.util());
+		cont3.releaseReusable(r2);
+		//si tratamos de eleminar r1 que ya esta eliminado deberia de saltar una excepción
+		//Assert.assertNotEquals(r1.util(), r2.util());
 	}
 
 }
