84fdb494daa7db9a71feaf8400a66a38d52e34b0
humbertoms99 <37192900+humbertoms99@users.noreply.github.com>
2021-03-06 16:59:01 +0100

    Cambios clase ReusablePoolTest
    Hemos cambiado el tratamiento de excepciones en todos los métodos de la clase ReusablePoolTest (Ahora lo hacemos con try - catch), lo que nos ha permitido tratar más posibles casos.

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -6,7 +6,6 @@ package ubu.gii.dass.test.c01;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,19 +15,23 @@ import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
 
 /**
- * @author alumno
- *
+ * @author Elsa Tolin Carrasco
+ * @author Humberto Marijuán Santamaría
+ * @author Roberto Arasti Blanco
  */
 public class ReusablePoolTest {
 	
 	ReusablePool contenedor = null;
-	Reusable r1,r2;
+	Reusable r1,r2,r3;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
 		contenedor = ReusablePool.getInstance();
+		r1 = null;
+		r2 = null;
+		r3 = null;
 	}
 
 	/**
@@ -37,8 +40,6 @@ public class ReusablePoolTest {
 	@After
 	public void tearDown() throws Exception {
 		contenedor = null;
-		r1 = null;
-		r2 = null;
 	}
 
 	/**
@@ -53,38 +54,49 @@ public class ReusablePoolTest {
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
-	 * @throws NotFreeInstanceException 
 	 */
 	@Test
-	public void testAcquireReusable() throws NotFreeInstanceException {
-		
-		
-		
-		r1 = contenedor.acquireReusable();
-		assertNotNull(r1);
-		r2 = contenedor.acquireReusable();
-		assertNotNull(r2);
-		//este método devuelve una excepcion
-		//assertError(contenedor.acquireReusable());
+	public void testAcquireReusable()  {
+		try {
+			r1 = contenedor.acquireReusable();
+			assertNotNull(r1);
+			r2 = contenedor.acquireReusable();
+			assertNotNull(r2);
+		}catch(NotFreeInstanceException e1) {
+			fail("Fallo por la Excepción NotFreeInstanceException");
+		}		
+		try {
+			r3 = contenedor.acquireReusable();
+		}catch(NotFreeInstanceException e) {
+			r3 = null; 
+		}
+		assertNull(r3); 
 	}
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
-	 * @throws NotFreeInstanceException 
-	 * @throws DuplicatedInstanceException 
 	 */
 	@Test
-	public void testReleaseReusable() throws NotFreeInstanceException, DuplicatedInstanceException {
-		
-		r1 = contenedor.acquireReusable();
-		String r1util = r1.util(); //guardamos su r1.util()
-		assertNotNull(r1);
-		contenedor.releaseReusable(r1);//si volvemos a crear otro deberia coincidir so .util
-		r2 = contenedor.acquireReusable();
-		assertNotNull(r1);
-		Assert.assertEquals(r1util, r2.util());
-		contenedor.releaseReusable(r2);
-		//si tratamos de eleminar r1 que ya esta eliminado deberia de saltar una excepción
+	public void testReleaseReusable(){
+		try {
+			r1 = contenedor.acquireReusable();
+			String r1util = r1.util(); 
+			assertNotNull(r1);
+			contenedor.releaseReusable(r1);
+			r2 = contenedor.acquireReusable();
+			assertNotNull(r1);
+			assertEquals(r1util, r2.util());
+			contenedor.releaseReusable(r2);
+		}catch(NotFreeInstanceException e1) {
+			fail("Fallo por la Excepción NotFreeInstanceException");
+		}catch(DuplicatedInstanceException e2) {
+			fail("Fallo por la Excepción DuplicatedInstanceException");
+		}
+		try {
+			contenedor.releaseReusable(r1); 
+		}catch(DuplicatedInstanceException e3) {
+			assertEquals(e3.getMessage(),"Ya existe esa instancia en el pool.");
+		}
 		
 	}
 
